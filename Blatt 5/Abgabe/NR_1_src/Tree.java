995b5d87e74d3958289a2fef3b691ff65f0cf530
Markus Vieth <mavieth@students.uni-mainz.de>
2015-11-23 17:36:41 +0100

    foobar

--- Blatt 5/Abgabe/NR_1_src/Tree.java
+++ Blatt 5/Abgabe/NR_1_src/Tree.java
@@ -54,29 +54,34 @@ class Tree<T extends Comparable<T>> extends AbstractTree<T> {
 		
 		
 		
-		ArrayList<Integer> numbersOrigin;
-		Random random;
+		//ArrayList<Integer> numbersOrigin;
+		//Random random;
 		
-		int sum;
+		//int[] sum;
 		
 		for (int k = 2; k <= 6; k++ ) {
-			sum = 0;
-			numbersOrigin = new ArrayList<Integer>();
-			random = new Random();
-			int nMax = (int)Math.pow(10.0, (double)k);
+			final int[] sum = new int[10];
+			//numbersOrigin = new ArrayList<Integer>();
+			//random = new Random();
+			final int nMax = (int)Math.pow(10.0, (double)k);
 			
-			for (int n=1;n <nMax;n++ ) {
-				numbersOrigin.add(n);
+			//for (int n=1;n <nMax;n++ ) {
+				//numbersOrigin.add(n);
 				//int ranNum = random.nextInt(nMax);
 				//if (tree.isIn(ranNum))
 					//n--;
 				//else
 					//tree.insert(ranNum);
-			}
+			//}
 			
 			
-			for (int i = 0; i < 10; i++) {
+			/*for (int i = 0; i < 10; i++) {
 				//Tree<Integer> tree = new Tree<Integer>(random.nextInt(nMax));
+				ArrayList<Integer> numbers = new ArrayList<Integer>(numbersOrigin);
+				int index = random.nextInt(numbers.size());
+				Tree<Integer> tree = new Tree<Integer>(numbers.get(index));
+				numbers.remove(index);
+				System.out.print(";");//Tree<Integer> tree = new Tree<Integer>(random.nextInt(nMax));
 				ArrayList<Integer> numbers = new ArrayList<Integer>(numbersOrigin);
 				int index = random.nextInt(numbers.size());
 				Tree<Integer> tree = new Tree<Integer>(numbers.get(index));
@@ -93,11 +98,67 @@ class Tree<T extends Comparable<T>> extends AbstractTree<T> {
 				System.out.print(".");//tree.maxDepth());
 				sum +=tree.maxDepth();
 				System.out.print("+");
+				
+				//for(int n = 1; n < nMax; n++) {
+				while(numbers.size()>0) {
+					index = random.nextInt(numbers.size());
+					tree.insert(numbers.get(index));
+					numbers.remove(index);
+				//}
+				}
+				System.out.print(".");//tree.maxDepth());
+				sum +=tree.maxDepth();
+				System.out.print("+");
 			}
+			*/
+			Thread[] threads = new Thread[10];
+			for (int i = 0; i < 10; i++) {
+				final int j = i;
+				threads[i] = new Thread(new Runnable() {
+					@Override
+					public void run() {
+
+						Random random = new Random();
+						ArrayList<Integer> numbers = new ArrayList<Integer>();
+						for (int n=1;n <nMax;n++ ) {
+							numbers.add(n);
+						}
+						
+						int index = random.nextInt(numbers.size());
+						Tree<Integer> tree = new Tree<Integer>(numbers.get(index));
+						numbers.remove(index);
+						System.out.print(";");
+						
+						//for(int n = 1; n < nMax; n++) {
+						while(numbers.size()>0) {
+							index = random.nextInt(numbers.size());
+							tree.insert(numbers.get(index));
+							numbers.remove(index);
+						//}
+						}
+						System.out.print(".");//tree.maxDepth());
+						sum[j] =tree.maxDepth();
+						System.out.print("+");
+												
+					}
+					
+				});
+				threads[i].start();
+			}
+			for (Thread thread: threads)
+				try {
+					thread.join();
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			
+			int result = 0;
+			for (int i: sum)
+				result += i;
 			
 			System.out.println();
-			System.out.println("k = "+k+" durchschnittliche Tiefe = "+sum/10);
+			System.out.println("k = "+k+" durchschnittliche Tiefe = "+result/10);
 		}
 		
 	}
