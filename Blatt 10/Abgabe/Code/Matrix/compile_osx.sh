1ea6fe090ffafb6ed72c8a80f2e7aa402744a705
David <dklopp@students.uni-mainz.de>
2016-01-12 13:35:14 +0100

    Added images to VL21§

--- Blatt 10/Abgabe/Code/Matrix/compile_osx.sh
+++ Blatt 10/Abgabe/Code/Matrix/compile_osx.sh
@@ -1,1 +1,1 @@
-/Users/David/Documents/UNI/3. Semester/DSeA/Abgaben/Blatt 9/Abgabe/Emmentaler_David/compile_osx.sh
+javac -source 1.6 -target 1.6 -bootclasspath '/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Classes/classes.jar' -cp '.:lwjgl/jar/*' app/App.java
