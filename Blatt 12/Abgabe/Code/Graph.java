11e60c60e8a6d0225c891b2c0b896cd1bfe328d9
Markus Vieth <mavieth@students.uni-mainz.de>
2016-01-28 22:10:15 +0100

    Zusatzaufgabe läuft

--- Blatt 12/Abgabe/Code/Graph.java
+++ Blatt 12/Abgabe/Code/Graph.java
@@ -1,5 +1,3 @@
-package fordFulkerson;
-
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
@@ -11,17 +9,20 @@ class Edge {
 	Vertex end;
 	Edge partner;
 	boolean isRest;
+    Vertex start;
 
-	public Edge (Vertex end, int capacity, boolean isRest, int cap) {
-		this.capacity = capacity;
+	public Edge (Vertex start, Vertex end, int capacity, boolean isRest, int cap) {
+
+        this.capacity = capacity;
 		this.CAP = cap;
 		this.end = end;
 		this.isRest = isRest;
+        this.start = start;
 	}
 
-	public int getCapacity() {
+	/*public int getCapacity() {
 		return this.capacity;
-	}
+	}*/
 
 	public Vertex getEnd() {
 		return this.end;
@@ -30,6 +31,12 @@ class Edge {
 	public void setPartner(Edge partner) {
 		this.partner = partner;
 	}
+
+    public String toString() {
+        if (isRest)
+            return end+"-"+start+'\t'+capacity+"/"+CAP;
+        return start+"-"+end+'\t'+capacity+"/"+CAP;
+    }
 }
 
 class Vertex {
@@ -39,14 +46,14 @@ class Vertex {
 	int id;
 
 	public Vertex() {
-		this.edges = new Vector<Edge>();
-		this.restEdges = new Vector<Edge>();
+		this.edges = new Vector<>();
+		this.restEdges = new Vector<>();
 		this.id = idCount++;
 	}
 
 	public void addEdge(Vertex end, int capacity, int used) {
-		Edge edge = new Edge(end, capacity-used, false, capacity);
-		Edge restEdge = new Edge(this, used, true, 0);
+		Edge edge = new Edge(this, end, capacity-used, false, capacity);
+		Edge restEdge = new Edge(end, this, used, true, capacity);
 		this.edges.add(edge);
 		end.restEdges.add(restEdge);
 		edge.setPartner(restEdge);
@@ -58,13 +65,13 @@ class Vertex {
 	}
 
 	public Edge getEdge(Vertex end) {
-		for ( int i = 0; i < edges.size(); i++) {
-			if (edges.get(i).end == end)
-				return edges.get(i);
+		for ( Edge e : edges) {
+			if (e.end == end)
+				return e;
 		}
-		for ( int i = 0; i < restEdges.size(); i++) {
-			if (restEdges.get(i).end == end)
-				return restEdges.get(i);
+		for ( Edge e : restEdges) {
+			if (e.end == end)
+				return e;
 		}
 
 		return null;
@@ -78,31 +85,36 @@ class Vertex {
 
 	@Override
 	public boolean equals(Object o){
-		if (o != null) {
-			return this.hashCode() == o.hashCode();
-		}
+        if (o instanceof Vertex) {
+            return this.hashCode() == o.hashCode();
+        }
 		return false;
 	}
+
+    public String toString() {
+        return id+"";
+    }
 }
 
 public class Graph {
 	Vector<Vertex> vertexs;
 
 	public Graph() {
-		this.vertexs = new Vector<Vertex>();
+        Vertex.idCount = 0;
+		this.vertexs = new Vector<>();
 	}
 
-	public Graph(Vector<Vertex> vertexs) {
+	/*public Graph(Vector<Vertex> vertexs) {
 		this.vertexs = vertexs;
-	}
+	}*/
 
 	public boolean addVertex() {
 		return vertexs.add(new Vertex());
 	}
 
-	public boolean addVertex(Vertex vertex) {
+	/*public boolean addVertex(Vertex vertex) {
 		return vertexs.add(vertex);
-	}
+	}*/
 
 	public void addEdge(int start, int end, int capacity) {
 		vertexs.get(start).addEdge(vertexs.get(end), capacity);
@@ -114,11 +126,11 @@ public class Graph {
 
 	private LinkedList<Vertex> findPath(int start, int end) {
 		if (start == end)
-			return new LinkedList<Vertex>();
+			return new LinkedList<>();
 
-		HashSet<Vertex> visited = new HashSet<Vertex>();
-		LinkedList<Vertex> stack = new LinkedList<Vertex>();
-		HashMap<Vertex,Vertex> pi = new HashMap<Vertex,Vertex>();
+		HashSet<Vertex> visited = new HashSet<>();
+		LinkedList<Vertex> stack = new LinkedList<>();
+		HashMap<Vertex,Vertex> pi = new HashMap<>();
 
 		Vertex origin = this.vertexs.get(start);
 		Vertex sink = this.vertexs.get(end);
@@ -129,7 +141,7 @@ public class Graph {
 		stack.push(origin);
 
 		while(stack.size() > 0) {
-			//System.out.println("Der Stack ist noch "+stack.size()+" Elemente gro�!");
+			//System.out.println("Der Stack ist noch "+stack.size()+" Elemente groß!");
 			Vertex u = stack.pop();
 			Vector<Edge> edges = u.edges;
 			edges.addAll(u.restEdges);
@@ -144,7 +156,7 @@ public class Graph {
 					stack.push(next);
 				}
 				if(next == sink) {
-					LinkedList<Vertex> temp = new LinkedList<Vertex>();
+					LinkedList<Vertex> temp = new LinkedList<>();
 					temp.push(next);
 					while (pi.get(next) != null) {
 						//System.out.println(next.id);
@@ -159,6 +171,59 @@ public class Graph {
 		return null;
 	}
 
+	public Vector<Edge> findMinCut(int start, int end) {
+        this.fordFulkerson(start, end);
+
+        if (start == end)
+            return new Vector<>();
+
+        HashSet<Vertex> visited = new HashSet<>(); //entspricht S
+        LinkedList<Vertex> queue = new LinkedList<>();
+        //HashMap<Vertex,Vertex> pi = new HashMap<>();
+
+        Vertex origin = this.vertexs.get(start);
+
+        visited.add(origin);
+        //pi.put(origin, null);
+
+        queue.add(origin);
+
+        while(queue.size() > 0) {
+            //System.out.println("Der Stack ist noch "+stack.size()+" Elemente groß!");
+            Vertex u = queue.poll();
+            Vector<Edge> edges = u.edges;
+            edges.addAll(u.restEdges);
+            for (int i = 0; i < edges.size(); i++) {
+                Edge toNext = edges.get(i);
+                if (toNext.capacity <= 0)
+                    continue;
+                Vertex next = toNext.getEnd();
+                if(!visited.contains(next)) {
+                    visited.add(next);
+                    //pi.put(next, u);
+                    queue.add(next);
+                }
+            }
+        }
+        Vector<Vertex> setT = new Vector<>();
+        for( Vertex v : this.vertexs) {
+            if(!visited.contains(v))
+                setT.add(v);
+        }
+        Vector<Edge> cut = new Vector<>();
+        for( Vertex v: visited) {
+            for (Vertex u : setT) {
+                Edge temp = v.getEdge(u);
+                if (temp!=null)
+                    cut.add(temp);
+            }
+        }
+
+        return cut;
+
+
+    }
+
 	public void fordFulkerson(int start, int end) {
 		if (start == end){
 			System.out.println("Start == End");
@@ -167,7 +232,7 @@ public class Graph {
 		LinkedList<Vertex> p = this.findPath(start, end);
 		while(p != null){
 			int c = Integer.MAX_VALUE;
-			LinkedList<Edge> pTemp = new LinkedList<Edge>(); 
+			LinkedList<Edge> pTemp = new LinkedList<>();
 			Vertex last = p.removeFirst();
 			while (!p.isEmpty()) {
 				Vertex next = p.removeFirst();
@@ -191,27 +256,34 @@ public class Graph {
 		StringBuilder out = new StringBuilder();
 		out.append("from\\to\t");
 		for (int i = 0; i < this.vertexs.size(); i++) {
+            out.append("  ");
 			out.append(i);
+            out.append("  ");
 			out.append('\t');
 		}
 		out.append('\n');
 		for (int i = 0; i < this.vertexs.size(); i++) {
-			out.append(i);
+            out.append("  ");
+            out.append(i);
+            out.append("  ");
 			out.append('\t');
 			Vertex u = this.vertexs.get(i);
 			for (int j = 0; j < this.vertexs.size(); j++) {
 				Vertex v = this.vertexs.get(j);
 				Edge edge = u.getEdge(v);
 				if (i == j ) {
-					out.append('-');
-					out.append('\t');					
+					out.append("  -  ");
+					out.append('\t');
 				}else	if (edge != null && !edge.isRest) {
+                    out.append(" ");
 					out.append(edge.CAP-edge.capacity);
 					out.append('/');
 					out.append(edge.CAP);
 					out.append('\t');
 				} else {
+                    out.append("  ");
 					out.append('-');
+                    out.append("  ");
 					out.append('\t');
 				}
 			}
@@ -240,38 +312,48 @@ public class Graph {
 		test.addEdge(5, 7, 11, 3);
 		test.addEdge(6, 0, 13, 12);
 		test.addEdge(6, 2, 13, 9);
-		test.addEdge(6, 4, 13, 0);
-		System.out.println("Ausgangssituation");
+        test.addEdge(6, 4, 13, 0);
+        System.out.println("Ausgangssituation");
 		test.print();
-		
-		test.fordFulkerson(6, 7);
-		System.out.println("Ergebnis");
+
+        test.fordFulkerson(6, 7);
+        System.out.println("Ergebnis");
 		
 		test.print();
+
+        System.out.println("Minimaler Schnitt");
+        Vector<Edge> cut = test.findMinCut(6,7);
+        for (Edge e : cut)
+            System.out.println(e.toString());
 		
 
 		System.out.println("Tailaufgabe d)");
 		test = new Graph();
 		for (int i = 0; i < 8; i++)
 			test.addVertex();
-		test.addEdge(0, 1, 1, 0);
-		test.addEdge(0, 3, 1, 0);
-		test.addEdge(1, 7, 1, 0);
-		test.addEdge(2, 3, 1, 0);
-		test.addEdge(2, 5, 1, 0);
-		test.addEdge(3, 7, 1, 0);
-		test.addEdge(4, 5, 1, 0);
-		test.addEdge(5, 7, 1, 0);
-		test.addEdge(6, 0, 1, 0);
-		test.addEdge(6, 2, 1, 0);
-		test.addEdge(6, 4, 1, 0);
-		System.out.println("Ausgangssituation");
+		test.addEdge(0, 1, 1);
+		test.addEdge(0, 3, 1);
+		test.addEdge(1, 7, 1);
+		test.addEdge(2, 3, 1);
+		test.addEdge(2, 5, 1);
+		test.addEdge(3, 7, 1);
+		test.addEdge(4, 5, 1);
+		test.addEdge(5, 7, 1);
+		test.addEdge(6, 0, 1);
+		test.addEdge(6, 2, 1);
+        test.addEdge(6, 4, 1);
+        System.out.println("Ausgangssituation");
 		
 		test.print();
-		
-		test.fordFulkerson(6, 7);
-		System.out.println("Ergebnis");
+
+        test.fordFulkerson(6, 7);
+        System.out.println("Ergebnis");
 		
 		test.print();
+
+        System.out.println("Minimaler Schnitt");
+        cut = test.findMinCut(6,7);
+        for (Edge e : cut)
+            System.out.println(e.toString());
 	}
 }
